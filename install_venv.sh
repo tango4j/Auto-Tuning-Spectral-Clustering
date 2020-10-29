ced85bb123c852e83cfbd5984dacaa7358102d47
Tae Jin Park <taejinpa@usc.edu>
2020-10-28 21:53:03 -0700

    Fixed install script issues

--- install_venv.sh
+++ install_venv.sh
@@ -1,5 +1,7 @@
-virtualenv -p $(which python3) $1
-source $PWD/"$1"/bin/activate
+venv_name=env_nmesc
+virtualenv -p $(which python3) $venv_name
+source $PWD/"$venv_name"/bin/activate
 pip3 install -U scikit-learn==0.22
 pip3 install kaldi_io==0.9.1
-pip3 ipdb
+pip3 install pyamg
+pip3 install ipdb
